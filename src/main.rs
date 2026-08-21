20eeee7a5a8d60569ddde39a6ddb2c46aaa3dfac
Alan Vardy <alanvardy@gmail.com>
2026-08-21 10:43:08 -0700

    fix(ci): add test and Cargo.lock to unblock CI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {}", greeting());
+}
+
+fn greeting() -> String {
+    "Hello, world!".to_string()
+}
+
+#[test]
+fn test_greeting() {
+    assert_eq!(greeting(), "Hello, world!");
 }
