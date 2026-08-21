ba5972b644cb602afefe6511d30849a4c0eb5ac6
Alan Vardy <alanvardy@gmail.com>
2026-08-21 12:03:13 -0700

    Review feedback: serve on port 3000 and log the hosting URL

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let state = app::state::AppState {
         templates: app::templates::init(),
     };
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    println!("Hosting on http://localhost:3000");
     axum::serve(
         listener,
         interfaces::routes::routes()
